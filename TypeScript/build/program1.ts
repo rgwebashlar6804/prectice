2222d682e5f3d2e5b15da349ee1ef2ade483c9ed
Anand-wa <100343082+anand-wa@users.noreply.github.com>
2024-03-23 15:44:30 +0530

    abc commit

--- TypeScript/build/program1.ts
+++ TypeScript/build/program1.ts
@@ -37,39 +37,158 @@
 // // sh
 // document.writeln(item_1.name);
 
-let tupleexp = [251,"Hello"];
-console.log(tupleexp);
+// let tupleexp = [251,"Hello"];
+// console.log(tupleexp);
 
-//GENERIC 
-function identity<T>(arg : T) :T {
-    return arg;
-}
+// //GENERIC 
+// function identity<T>(arg : T) :T {
+//     return arg;
+// }
 
-let result1 = identity<boolean | number >(true);
-console.log('====================================');
-console.log(typeof result1);
-console.log('====================================');
-[1, 2, 3].map(value => value.toString());   
-
-
-var list : Array<number> = [1, 3, 5];  
-console.log(list);
+// let result1 = identity<boolean | number >(true);
+// console.log('====================================');
+// console.log(typeof result1);
+// console.log('====================================');
+// [1, 2, 3].map(value => value.toString());   
 
 
-//alias 
+// var list : Array<number> = [1, 3, 5];  
+// console.log(list);
 
-type User = {
+
+// //alias 
+
+// type User = {
+//     name: string,
+//     age: number,
+//     addres?: string //yaa to string hoi ka undefined hoi che 
+// }
+
+// const user: User = {
+//     name: "ronak",
+//     age: 12
+// }
+
+// function login(userData: User): User{
+
+//     return userData;
+// }
+
+// type Id = number | string;
+// const userId: Id = '12';
+// console.log(userId);
+
+//interface
+
+// interface Book{
+//     name: string;
+//     price: number;
+// }
+// interface Ebook extends Book{
+//     fileSize :number;
+//     format: string;
+// }
+// interface audioBook extends Ebook{
+//     duration :number;
+// }
+
+// const book: audioBook = {
+//     name: 'atomic habit',
+//     price: 200,
+//     fileSize: 10,
+//     format: "pdf",
+//     duration: 4
+// }
+
+
+// console.log(book);
+
+
+// //interface merging 
+
+// interface product{
+//     name: string;
+//     category: string;
+//     price:number;
+// }
+
+// interface product{
+//     description : string
+// } 
+
+
+// const productData: product = {
+//     name: 'nike cap',
+//     category:'cap',
+//     price: 234,
+//     description: 'this is a super cap'
+// }
+
+// console.log(productData);
+
+
+// const enum Animals {
+//     Fish
+// }
+
+// // console.log(Animals.Fish);
+// var Animals;
+// (function (Animals) {
+//     Animals[Animals["Fish"] = 0] = "Fish";
+// })(Animals || (Animals = {}));
+// console.log(Animals.Fish);
+// console.log(0);
+
+// let a:void=undefined;
+// a=3;//error
+// let b:undefined;
+// console.log(b);
+
+// let Fname: any = "RONAK";
+// let namelength: number = (Fname as string).length;
+// console.log(Fname);
+// console.log(namelength);
+
+// type userData = {username: string, points: number};
+// let demoUser: userData;
+// demoUser = 
+// {
+//     username: 'John',
+//     points: 10
+// };
+
+// console.log(demoUser);
+// type User = 
+// {
+//     name: string;
+// }
+
+// type Organisation = 
+// {
+//     name: string;
+//     manager: User;
+// };
+
+// const manager: User = 
+// {
+//     name: 'Nick',
+// }
+
+// const org: Organisation = 
+// {
+//     name: 'ABC',
+//     manager,
+// }
+
+let Order :string[];
+
+type Person = 
+{
+    id: number,
     name: string,
-    age: number,
-    addres?: string //yaa to string hoi ka undefined hoi che 
+    yearOfBirth: number,
+    favouriteMovies?: string[], 
+    orders?: Order[]
 }
+console.log(Person);
 
-const user: User = {
-    name: "ronak",
-    age: 12
-}
-
-function login(userData: User): User{
-
-    return userData;
-}
--- TypeScript/src/program1.js
+++ TypeScript/src/program1.js
@@ -30,23 +30,114 @@
 // console.log(item_1.sayTotal());
 // // sh
 // document.writeln(item_1.name);
-let tupleexp = [251, "Hello"];
-console.log(tupleexp);
-//GENERIC 
-function identity(arg) {
-    return arg;
-}
-let result1 = identity(true);
-console.log('====================================');
-console.log(typeof result1);
-console.log('====================================');
-[1, 2, 3].map(value => value.toString());
-var list = [1, 3, 5];
-console.log(list);
-const user = {
-    name: "ronak",
-    age: 12
-};
-function login(userData) {
-    return userData;
-}
+// let tupleexp = [251,"Hello"];
+// console.log(tupleexp);
+// //GENERIC 
+// function identity<T>(arg : T) :T {
+//     return arg;
+// }
+// let result1 = identity<boolean | number >(true);
+// console.log('====================================');
+// console.log(typeof result1);
+// console.log('====================================');
+// [1, 2, 3].map(value => value.toString());   
+// var list : Array<number> = [1, 3, 5];  
+// console.log(list);
+// //alias 
+// type User = {
+//     name: string,
+//     age: number,
+//     addres?: string //yaa to string hoi ka undefined hoi che 
+// }
+// const user: User = {
+//     name: "ronak",
+//     age: 12
+// }
+// function login(userData: User): User{
+//     return userData;
+// }
+// type Id = number | string;
+// const userId: Id = '12';
+// console.log(userId);
+//interface
+// interface Book{
+//     name: string;
+//     price: number;
+// }
+// interface Ebook extends Book{
+//     fileSize :number;
+//     format: string;
+// }
+// interface audioBook extends Ebook{
+//     duration :number;
+// }
+// const book: audioBook = {
+//     name: 'atomic habit',
+//     price: 200,
+//     fileSize: 10,
+//     format: "pdf",
+//     duration: 4
+// }
+// console.log(book);
+// //interface merging 
+// interface product{
+//     name: string;
+//     category: string;
+//     price:number;
+// }
+// interface product{
+//     description : string
+// } 
+// const productData: product = {
+//     name: 'nike cap',
+//     category:'cap',
+//     price: 234,
+//     description: 'this is a super cap'
+// }
+// console.log(productData);
+// const enum Animals {
+//     Fish
+// }
+// // console.log(Animals.Fish);
+// var Animals;
+// (function (Animals) {
+//     Animals[Animals["Fish"] = 0] = "Fish";
+// })(Animals || (Animals = {}));
+// console.log(Animals.Fish);
+// console.log(0);
+// let a:void=undefined;
+// a=3;//error
+// let b:undefined;
+// console.log(b);
+// let Fname: any = "RONAK";
+// let namelength: number = (Fname as string).length;
+// console.log(Fname);
+// console.log(namelength);
+// type userData = {username: string, points: number};
+// let demoUser: userData;
+// demoUser = 
+// {
+//     username: 'John',
+//     points: 10
+// };
+// console.log(demoUser);
+// type User = 
+// {
+//     name: string;
+// }
+// type Organisation = 
+// {
+//     name: string;
+//     manager: User;
+// };
+// const manager: User = 
+// {
+//     name: 'Nick',
+// }
+// const org: Organisation = 
+// {
+//     name: 'ABC',
+//     manager,
+// }
+let Order;
+console.log(Person);
